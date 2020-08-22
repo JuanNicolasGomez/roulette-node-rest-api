b8a703a992d59fcfa1f36a73881136b561675a9c
Nicolas Gomez <juan.gomez@masivian.com>
2020-08-21 20:33:47 -0500

    [add] open roulete method

--- src/routes/roulettes.js
+++ src/routes/roulettes.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 const roulettes = require('../roulettes_mock.json');
-console.log(roulettes)
+
 
 router.get('/', (req,res) => {
     res.json(roulettes);
@@ -10,7 +10,6 @@ router.get('/', (req,res) => {
 router.post('/', (req,res) => {
     if (isValidBody(req.body)){
         const newRoulette = buildRouletteBody(req.body);
-        console.log(newRoulette);
         roulettes.push(newRoulette);
         res.json({
             "id":newRoulette.id
@@ -21,12 +20,34 @@ router.post('/', (req,res) => {
     
 })
 
+router.patch('/:id', (req,res) => {
+    const {id} = req.params;
+    if (openRoulette(id)){
+        res.json({"id": id, "desc":"Roulette with id " + id + " opened succesfully."})
+    }
+    else{
+        res.status(500).json({error: "Roullette could not be opened"});
+    }
+})
+
 
 function buildRouletteBody(body){
     const id = roulettes.length + 1;
     const state = 'closed';
     const newRoulette = {...body, id, state}
     return newRoulette;
+}
+
+function openRoulette(id){
+    var state = false;
+    roulettes.forEach(roulette => {
+        if (roulette.id == id && roulette.state == 'closed'){
+            roulette.state = 'open'
+            state =  true;
+        }
+        console.log(roulette)
+    });
+    return state;
 }
 
 function isValidBody(body){
