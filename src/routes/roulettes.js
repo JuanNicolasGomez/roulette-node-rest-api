2afd8bdee6de0a198a584f9eca91047d97766484
Nicolas Gomez <juan.gomez@masivian.com>
2020-08-21 20:14:03 -0500

    [refactor] post method

--- src/routes/roulettes.js
+++ src/routes/roulettes.js
@@ -8,19 +8,30 @@ router.get('/', (req,res) => {
 })
 
 router.post('/', (req,res) => {
-    const {name} = req.body;
-    if (name){
-        const id = roulettes.length + 1;
-        const state = 'closed';
-        const newRoulette = {...req.body, id, state}
+    if (isValidBody(req.body)){
+        const newRoulette = buildRouletteBody(req.body);
+        console.log(newRoulette);
         roulettes.push(newRoulette);
         res.json({
-            "id":id.toString()
+            "id":newRoulette.id
         });
     }else{
         res.status(500).json({error: "Wrong request"});
     }
     
 })
+
+
+function buildRouletteBody(body){
+    const id = roulettes.length + 1;
+    const state = 'closed';
+    const newRoulette = {...body, id, state}
+    return newRoulette;
+}
+
+function isValidBody(body){
+    const {name} = body;
+    return name;
+}
 
 module.exports = router;
